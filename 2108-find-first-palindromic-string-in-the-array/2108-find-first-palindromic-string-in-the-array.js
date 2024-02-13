a476a8cf6a35fd4d7cbd4a8fb573bd660ea14bf9
Jae Hwan, Kim <111283378+darren-kk@users.noreply.github.com>
2024-02-13 13:08:40 +0900

    Time: 85 ms (49.23%), Space: 57.3 MB (11.15%) - LeetHub

---
+++ 2108-find-first-palindromic-string-in-the-array/2108-find-first-palindromic-string-in-the-array.js
@@ -0,0 +1,13 @@
+/**
+ * @param {string[]} words
+ * @return {string}
+ */
+var firstPalindrome = function(words) {
+  for (const word of words) {
+    if (word === word.split("").reverse().join("")) {
+      return word;
+    }
+  }
+  
+  return "";
+};
